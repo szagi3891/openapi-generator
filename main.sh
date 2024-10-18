749fb0d4a0856e8f9e5c02d1dc1ea357f7fa7c59
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-10-18 13:20:22 +0200

    docs

--- main.sh
+++ main.sh
@@ -1,1 +1,6 @@
 deno run -A ./main.ts ./example/spec_openapi.json ./example/target
+
+
+# deno run -A https://raw.githubusercontent.com/szagi3891/openapi-generator/refs/heads/main/main.ts
+
+# deno run -A --import-map=https://raw.githubusercontent.com/szagi3891/openapi-generator/refs/heads/main/deno.json https://raw.githubusercontent.com/szagi3891/openapi-generator/refs/heads/main/main.ts
